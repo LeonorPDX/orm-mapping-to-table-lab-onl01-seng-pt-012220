b50db80caf48ebe757fc21b1924213445d72f40b
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-30 20:50:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,11 +14,18 @@ class Student
   end
   
   def self.create_table
-    
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
   
   def self.drop_table
-    
+    DB[:conn].execute("DROP TABLE students;") 
   end
   
 end
